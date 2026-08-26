9c3b9122ed58dff44a3c80abc23bd3b947621bb7
donaldiorwua <donaldiorwua@gmail.com>
2026-08-26 12:53:44 +0100

    added maximum number of attempts logic

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def main():
     print("I'm thinking of a number between 1 and 100.")
 
     number = random.randint(1, 100)
+    attempts = 0
+    max_attempts = 5
     
-    while True:
+    while attempts < max_attempts:
         try:
             guess = int(input("Enter your guess: "))
+            attempts += 1
             if guess < 1 or guess > 100:
                 print("Please enter a number between 1 and 100.")
                 continue
@@ -18,6 +21,10 @@ def main():
             print(result)
             if result.startswith("Congratulations"):
                 break
+            if not result.startswith("Congratulations"):
+                print(f"You guessed the number in {attempts} attempts!, you have {max_attempts - attempts} attempts left.")
+            elif attempts == max_attempts:
+                print(f"You've used all {max_attempts} attempts. The number was: {number}!")
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
